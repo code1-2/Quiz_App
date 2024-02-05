b2a34d3359a755c7e323af7fe10017cbca9b2f5b
V000000000 <vishalmishra230899@gmail.com>
2024-02-05 22:31:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quiz_app/QuestionAnswer.java
@@ -0,0 +1,32 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String[] question = {
+
+            "What is 10+26 ?",
+            "who invented Telephone?",
+            "what is 129 ?",
+            "who is the founder of Spacex?",
+            "In the given options, which is the Example of System Software?"
+    };
+
+    public static String[][] choices = {
+
+            {"32", "42", "36", "38"},
+            {"Graham Bell", "Einstein", "Edison", "None of the above"},
+            {"96", "84", "102", "108"},
+            {"Jeff Bezos", "Elon Musk", "Steve Jobs", "Bill Gates"},
+            {"Windows", "Linux", "Macos", "All of the above"}
+    };
+
+
+    protected static String[] correctAnswer ={
+            "36",
+            "Graham Bell",
+            "108",
+            "Elon Musk",
+            "All of the above"};
+
+
+}
